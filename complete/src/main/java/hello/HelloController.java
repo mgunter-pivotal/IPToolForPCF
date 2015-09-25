533c71428c0a51dedab4e49d3f99bb3f6bd2b8ed
mgunter-pivotal <mgunter@pivotal.io>
2015-09-24 23:35:16 -0400

    created new testing application for PCF - it shows ip addresses for inbound and outbound

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -1,14 +1,60 @@
 package hello;
 
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @RestController
 public class HelloController {
-    
-    @RequestMapping("/")
+	String remoteResponse;
+	
+    @RequestMapping("/test")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from Asbury Park!";
     }
     
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public  String generateReport(
+            HttpServletRequest request, 
+            HttpServletResponse response) {
+
+    	return "---remote Address:>"+request.getRemoteAddr() +
+    			"---remote Host:>"+request.getRemoteHost() +
+    			"---context:>"+request.getContextPath();
+        // ...
+        // Here you can use the request and response objects like:
+        // response.setContentType("application/pdf");
+        // response.getOutputStream().write(...);
+
+    }
+    
+    
+    
+    @RequestMapping(value = "/reportoutbound/{url}", method = RequestMethod.GET)
+    public String  generateReport3(
+            @PathVariable("url") String url, 
+            HttpServletRequest request, 
+            HttpServletResponse response) {
+
+    	
+    	try {
+			remoteResponse = HttpURLConnectionExample.call("http://"+url+"/");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	return remoteResponse;
+        // ...
+        // Here you can use the request and response objects like:
+        // response.setContentType("application/pdf");
+        // response.getOutputStream().write(...);
+
+    }
 }
